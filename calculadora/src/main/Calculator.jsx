dc4b01337b1b8ff8f78585e302ddb43fe802e3d4
Daniel_jr <daniel@extradigital.com.br>
2019-02-22 15:52:23 -0300

    Display
    Criação do componente display e alterações de estilo na calculadora

--- calculadora/src/main/Calculator.jsx
+++ calculadora/src/main/Calculator.jsx
@@ -2,28 +2,50 @@ import React , { Component } from 'react'
 import './Calculator.css'
 
 import Button from '../components/Buttun'
+import Display from '../components/Display'
 
 export default class Calculator extends Component {
+
+    constructor(props) {
+        super(props)
+        this.clearMenmory = this.clearMenmory.bind(this)
+        this.setOperation = this.setOperation.bind(this)
+        this.addDigito = this.addDigito.bind(this)
+    }
+
+    clearMenmory() {
+       console.log('Limpar') 
+    }
+
+    setOperation(operation) {
+        console.log(operation) 
+    }
+
+    addDigito(n) {
+        console.log(n)
+    }
+
     render() {
         return (
-            <div className="calculator"> 
-                <Button label='AC' />
-                <Button label='/' />
-                <Button label='7' />
-                <Button label='8' />
-                <Button label='9' />
-                <Button label='+' />
-                <Button label='4' />
-                <Button label='5' />
-                <Button label='6' />
-                <Button label='-' />
-                <Button label='1' />
-                <Button label='2' />
-                <Button label='3' />
-                <Button label='+' />
-                <Button label='0' />
-                <Button label='.' />
-                <Button label='=' />                
+            <div className="calculator">
+                <Display value={100} /> 
+                <Button label="AC"  click={this.clearMenmory} triple/>
+                <Button label="/" click={this.setOperation} operation/>
+                <Button label="7" click={this.addDigito} />
+                <Button label="8" click={this.addDigito}/>
+                <Button label="9" click={this.addDigito}/>
+                <Button label="*" click={this.setOperation} operation/>
+                <Button label="4" click={this.addDigito}/>
+                <Button label="5" click={this.addDigito}/>
+                <Button label="6" click={this.addDigito}/>
+                <Button label="-" click={this.setOperation} operation/>
+                <Button label="1" click={this.addDigito}/>
+                <Button label="2" click={this.addDigito}/>
+                <Button label="3" click={this.addDigito}/>
+                <Button label="+" click={this.setOperation} operation/>
+                <Button label="0" click={this.addDigito} double/>
+                <Button label="." click={this.addDigito}/>
+                <Button label="=" click={this.setOperation} operation/>                
             </div> 
         )
     }
